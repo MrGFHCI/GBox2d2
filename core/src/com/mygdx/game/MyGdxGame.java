fe00669875a2e3e09300b9598eaedfa10ff50551
MrGFHCI <scott_grondin@wrdsb.on.ca>
2015-02-11 12:14:39 -0500

    pressed enter

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -17,6 +17,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 
+
 public class MyGdxGame extends ApplicationAdapter {
     SpriteBatch batch;
     Sprite sprite;
